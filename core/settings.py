5df24c03429fec50934d50d999db27a4d6b335a0
juliagmf <julia.jgmf@gmail.com>
2022-05-13 12:13:39 -0300

    add extensão

--- core/settings.py
+++ core/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    
+    'rest_framework', #framework
+    'olympic', #app
+    'django_extensions', # add extensão
 ]
 
 MIDDLEWARE = [
